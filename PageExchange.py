0cfc1ef84bc3916b6ba8c66303d1addbffd66e04
Berg <vandenberg.jutai@gmail.com>
2017-01-25 17:57:50 -0400

    Initial commit

---
+++ PageExchange.py
@@ -0,0 +1,30 @@
+import sys
+
+
+class Main():
+
+    def __init__(self):
+        pass
+
+    def fileToLst(self):
+        file = open(str(sys.argv[2]), "r")
+        str = read(file)
+
+    def read(self, file):
+        for line in file:
+            line = line.strip().split()
+            
+
+class PageExchange():
+
+    def __init__(self, lst):
+        self.lst = lst
+
+    def fifo(self):
+        pass
+
+    def lru(self):
+        pass
+
+    def secondChance(self):
+        pass
